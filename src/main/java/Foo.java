cee5e61dde0c00d513c1361ccb037e0694d9eba4
shfaradjli <Peace4us>
2021-08-04 23:26:15 +0400

    1th iteration

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Foo {
+
+    String convertTextToList(String text) {
+        List<String> wordArrayList = new ArrayList<>();
+        for (String word : text.split(" ")) {
+            wordArrayList.add(word);
+        }
+        wordArrayList.removeAll(Arrays.asList("", null));
+        return "Number of words: " + wordArrayList.size();
+    }
 }
