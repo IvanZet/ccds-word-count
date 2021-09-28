e304a9682e5286ab6f9fb0792d6d5a80ffa71f69
Bernhard <bernhard.fleck@gmail.com>
2021-09-28 10:55:18 +0200

    Revert "1th iteration"
    This reverts commit cee5e61dde0c00d513c1361ccb037e0694d9eba4.

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,15 +1,2 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Foo {
-
-    String convertTextToList(String text) {
-        List<String> wordArrayList = new ArrayList<>();
-        for (String word : text.split(" ")) {
-            wordArrayList.add(word);
-        }
-        wordArrayList.removeAll(Arrays.asList("", null));
-        return "Number of words: " + wordArrayList.size();
-    }
 }
