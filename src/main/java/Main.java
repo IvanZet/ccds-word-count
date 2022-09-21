999ddb08842c84050d675371ef75a3e9801cf3a6
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-09-21 09:26:07 +0200

    Cleanes lefover files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,7 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-
     public static void main(final String[] args) {
 
-        try (final BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in))) {
-            final WordCount wc = new WordCount();
-            System.out.print("Enter text: ");
-            String text = buffReader.readLine();
-            System.out.print("Number of words: " + wc.countWords(text));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
     }
-
-
 }
