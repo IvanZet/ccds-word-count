ce75948ab35abd0f210d28f1eb7e9957fb037faf
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-09-21 13:50:50 +0200

    Adds missing @Override annotation to counter and reader classes

--- src/main/java/hiring/wordcounter/counter/WordCounter.java
+++ src/main/java/hiring/wordcounter/counter/WordCounter.java
@@ -5,6 +5,7 @@ import hiring.wordcounter.main.Counter;
 import java.util.regex.Pattern;
 
 public class WordCounter implements Counter {
+    @Override
     public int count(String text) {
         int counter = 0;
         for (String word: text.split(" ")) {
--- src/main/java/hiring/wordcounter/reader/InputReader.java
+++ src/main/java/hiring/wordcounter/reader/InputReader.java
@@ -5,6 +5,7 @@ import hiring.wordcounter.main.Reader;
 import java.util.Scanner;
 
 public class InputReader implements Reader {
+    @Override
     public String read() {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Enter text:");
