f81afba089f419b380fd01851d317638116d5df0
Ivan Zykov <ivan.v.zykov@gmail.com>
2023-02-07 17:42:26 +0100

    Handles null user's input and stop words in CounterOfWords

--- src/main/java/net/ivanzykov/wordcounter/counterofwords/CounterOfWords.java
+++ src/main/java/net/ivanzykov/wordcounter/counterofwords/CounterOfWords.java
@@ -1,8 +1,11 @@
 package net.ivanzykov.wordcounter.counterofwords;
 
 import net.ivanzykov.wordcounter.runner.Actor;
+import net.ivanzykov.wordcounter.runner.FieldOfWordCountNullException;
 import net.ivanzykov.wordcounter.runner.WordCount;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class CounterOfWords implements Actor {
@@ -16,8 +19,22 @@ public class CounterOfWords implements Actor {
     @Override
     public void determineWordCount(WordCount wordCount) {
         Integer counter = 0;
-        for (String word: wordCount.getUsersInput().split(" ")) {
-            if (Pattern.matches("[a-z,A-Z]+", word) && ! wordCount.getStopWords().contains(word)) {
+        String errorMessageStart = "Failed to count words. ";
+        String errorMessageEnd = "is null in WordCount object";
+        String usersInput;
+        try {
+            usersInput = Objects.requireNonNull(wordCount.getUsersInput());
+        } catch (NullPointerException e) {
+            throw new FieldOfWordCountNullException(errorMessageStart + "User's input " + errorMessageEnd);
+        }
+        List<String> stopWords;
+        try {
+            stopWords = Objects.requireNonNull(wordCount.getStopWords());
+        } catch (NullPointerException e) {
+            throw new FieldOfWordCountNullException(errorMessageStart + "Stop words " + errorMessageEnd);
+        }
+        for (String word: usersInput.split(" ")) {
+            if (Pattern.matches("[a-z,A-Z]+", word) && ! stopWords.contains(word)) {
                 counter++;
             }
         }
--- src/test/java/net/ivanzykov/wordcounter/counterofwords/CounterOfWordsTest.java
+++ src/test/java/net/ivanzykov/wordcounter/counterofwords/CounterOfWordsTest.java
@@ -1,7 +1,10 @@
 package net.ivanzykov.wordcounter.counterofwords;
 
 import net.ivanzykov.wordcounter.WordCounterApp;
+import net.ivanzykov.wordcounter.runner.Actor;
+import net.ivanzykov.wordcounter.runner.FieldOfWordCountNullException;
 import net.ivanzykov.wordcounter.runner.WordCount;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -10,10 +13,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CounterOfWordsTest {
 
+    private WordCount wordCount;
+    private Actor counterOfWords;
+
+    @BeforeEach
+    void setUp() {
+        wordCount = new WordCount(WordCounterApp.STOP_WORDS_FILENAME);
+        counterOfWords = new CounterOfWords();
+    }
+
     @Test
-    void determineWordCount() {
+    void determineWordCount_setsWordCountInWordCount() {
         // Prepare args
-        WordCount wordCount = new WordCount(WordCounterApp.STOP_WORDS_FILENAME);
         wordCount.setStopWords(Arrays.asList("the", "a", "on", "off"));
         wordCount.setUsersInput("hello w1rld 3 # the a on off");
 
@@ -21,8 +32,26 @@ class CounterOfWordsTest {
         assertNull(wordCount.getCountOfWords());
 
         // Run this test
-        new CounterOfWords().determineWordCount(wordCount);
+        counterOfWords.determineWordCount(wordCount);
 
         assertEquals(1, wordCount.getCountOfWords());
+    }
+
+    @Test
+    void determineWordCount_nullUsersInput_throwsException() {
+        Exception exception = assertThrows(FieldOfWordCountNullException.class, () ->
+                counterOfWords.determineWordCount(wordCount));
+
+        assertEquals("Failed to count words. User's input is null in WordCount object", exception.getMessage());
+    }
+
+    @Test
+    void determineWordCount_nullStopWords_throwsException() {
+        wordCount.setUsersInput("abc");
+
+        Exception exception = assertThrows(FieldOfWordCountNullException.class, () ->
+                counterOfWords.determineWordCount(wordCount));
+
+        assertEquals("Failed to count words. Stop words is null in WordCount object", exception.getMessage());
     }
 }
